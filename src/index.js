00c1e3424da6120a3676ee323944dc47fced23a1
pat <pat@tilde.team>
2022-10-31 01:09:37 -0500

    use material table

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<p>Hello, world!</p>);
+import React from "react";
+import MaterialTable, { Column } from "@material-table/core";
+
+const lookup = { true: "Available", false: "Unavailable" };
+
+const columns = [
+  { title: "First Name", field: "firstName" },
+  { title: "Last Name", field: "lastName" },
+  { title: "Birth Year", field: "birthYear", type: "numeric" },
+  { title: "Availablity", field: "availability", lookup }
+];
+
+const data = [
+  { firstName: "Tod", lastName: "Miles", birthYear: 1987, availability: true },
+  { firstName: "Jess", lastName: "Smith", birthYear: 2000, availability: false }
+];
+
+export const App = () => <MaterialTable columns={columns} data={data} />;
